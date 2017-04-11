dc81b832cb43c037f89b8b7847498eb4a87839a6
Kay Krewson <kay.k@comcast.net>
2017-04-11 06:32:47 -0700

    wrapped the getall in a function

--- routes/movies.js
+++ routes/movies.js
@@ -2,21 +2,38 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/connection');
 
+router.get('/', getAllMovies('movies'));
 
-// ======== GET ALL MOVIES =========
-router.get('/', function(req, res, next) {
-  db('movies').select('*').then(movies => {
-    // var movieStr = JSON.stringify(movies)
-    res.render('movies/index', {
-      movies: movies[0].title,
-      director: movies[0].director,
-      year: movies[0].year,
-      poster: movies[0].poster_url
-    });
-  });
-});
+// ======== GET ALL MOVIES ========
+function getAllMovies() {
+  return function(req, res, next) {
+    db('movies').select('*').then(movies => {
+      res.render('movies/index', {
+        movies: movies[0].title,
+        director: movies[0].director,
+        year: movies[0].year,
+        rating: movies[0].my_rating,
+        poster: movies[0].poster_url
+      })
+    })
+  }
+}
 
-// ======== GET ONE MOVIE ========
+
+// ======== GET ONE MOVIE =========
+function getOneMovie() {
+  // router.get('/', function(req, res, next) {
+  //   db('movies').select('*').where('id', req.params.id) then(movies => {
+  //    res.render('movies/index', {
+  //    movies: movies[0].title,
+  //    director: movies[0].director,
+  //    year: movies[0].year,
+  //    rating: movies[0].my_rating,
+  //    poster: movies[0].poster_url
+  //     });
+  //   });
+  // });
+}
 
 
 // PUT /movies/
