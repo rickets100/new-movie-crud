20ed27a2cfd8863aefc0b765443e2d0c55dbdad2
Kay Krewson <kay.k@comcast.net>
2017-04-13 06:30:11 -0700

    made the partials-related changes from Wes' examples

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index)
 app.use('/movies', movies);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 // ======== GET HOME PAGE ========
 router.get('/', function(req, res, next) {
-  let temp = "Look at this page.";
-  res.render('index', { title: temp });
+  let splash = "Movies!";
+  res.render('index', { title: splash });
   // res.locals refers to the title temp thing above
 });
 
